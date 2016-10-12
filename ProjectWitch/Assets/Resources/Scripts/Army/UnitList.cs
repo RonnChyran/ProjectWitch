61114575194fb8d772ac5439eeafa658ce804afc
yuichi ishikawa <yuichi ishikawa>
2016-10-12 21:50:43 +0900

    surface 2016/10/12v2

---
+++ ProjectWitch/Assets/Resources/Scripts/Army/UnitList.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Army
+{
+    public class UnitList : MonoBehaviour
+    {
+        
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
