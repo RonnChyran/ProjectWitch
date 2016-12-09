d1e4a85c62abe8fca943512416b012cf46c60bad
yuichi ishikawa <yuichi ishikawa>
2016-12-09 21:21:05 +0900

    2016/12/9

---
+++ ProjectWitch/Assets/Resources/Scripts/Field/TalkCommandHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ProjectWitch.Field
+{
+    public class TalkCommandHelper : MonoBehaviour
+    {
+
+        //ある処理が終了したときに呼び出されるメソッド
+        public delegate void EndCallBack();
+
+        //指定エリアの位置にカメラを移動させてハイライトする
+        public void HilightArea(int area, EndCallBack callback)
+        {
+
+        }
+
+        //コルーチン
+
+        private void _HilightArea(int area, EndCallBack callback)
+        {
+
+            //終了通知
+            callback();
+        }
+    }
+}
